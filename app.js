d854a8e8fdc420d4761398d9af86568166fd3886
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2020-05-28 03:02:58 +0200

    wip

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 require('dotenv').config();
 const axios = require('axios');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+
+app.use(cors());
 
 const runApp = async () => {
     const res = await axios.get(`http://newsapi.org/v2/everything?q=bitcoin&from=2020-04-28&sortBy=publishedAt&apiKey=${process.env.NA_KEY}`);
-    console.log(res.data);
+    return res.data
 }
 
-runApp();
+app.get('/', async (req, res) => {
+    const resp = await axios.get(`http://newsapi.org/v2/everything?q=bitcoin&from=2020-04-28&sortBy=publishedAt&apiKey=${process.env.NA_KEY}`);
+    res.json(resp.data)
+});
+
+app.listen(3000, () => console.log('App started at port 3000'))
